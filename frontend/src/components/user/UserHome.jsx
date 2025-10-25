7df0e740f4f84d854cf2c30fa66a1fb93ba61fec
bharathi <reddybharathi599@gmail.com>
2025-10-25 18:14:21 +0530

    Fix: import DoctorHome in UserHome1

--- frontend/src/components/user/UserHome.jsx
+++ frontend/src/components/user/UserHome.jsx
@@ -12,7 +12,7 @@ import { useLocation } from "react-router-dom";
 import ApplyDoctor from './ApplyDoctor';
 import UserAppointments from './UserAppointments';
 import DoctorList from './DoctorList';
-
+import DoctorHome from './DoctorHome'; // Adjust the path if needed
 const UserHome = () => {
    const [doctors, setDoctors] = useState([]);
    const [userdata, setUserData] = useState(null); // Initialize as null
